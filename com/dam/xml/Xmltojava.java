c1af09cf3ec2f69c164394ac93bc65297f7fecda
vdamodhar <vaggudamu@gmail.com>
2024-07-24 14:46:48 -0700

    Updated

---
+++ com/dam/xml/Xmltojava.java
@@ -0,0 +1,13 @@
+package com.dam.xml;
+
+import java.io.File;
+
+public class Xmltojava {
+
+    public static void main(String[] args) {
+    
+        File file = new File("//resources//S-124FC_1.0_20230427.xml");
+
+    }
+    
+}
